0ede4ece9cdc97eb5fd30b52af771548eefb368c
fkond <fkond@yahoo.com>
2023-05-25 22:27:48 +0300

    set yapısı örnek yapılmadı

---
+++ bolum4/set_yapisi.dart
@@ -0,0 +1,5 @@
+// her biri birbirinden farklı elemanları saklar.
+// örneğin birden fazla defa başvuru yapan kişiye tek ödül verilecek ise set içine alınabilir. Böylece unique olur.
+void main(List<String> args) {
+  
+}
