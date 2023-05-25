dd81f4d6f391d69f14efc3f1ad3e0cbd589d200a
fkond <fkond@yahoo.com>
2023-05-25 19:51:06 +0300

    List giriş

---
+++ bolum4/buyuyen_listeler.dart
@@ -0,0 +1,30 @@
+import 'dart:math';
+
+void main(List<String> args) {
+  List<int?> sayilar = [];
+  sayilar.add(1);
+  sayilar.add(2);
+  sayilar.add(3);
+  sayilar.add(4);
+  print(sayilar);
+  print(sayilar.length);
+  sayilar.length = 100;
+
+  List<int> sayilar2 = [1, 2, 3];
+  sayilar2.add(11);
+  sayilar2.add(12);
+  sayilar2.add(13);
+  print(sayilar2);
+
+  List<int> sayilar3 = List.filled(10, 10, growable: true);
+  sayilar3.add(11);
+  print(sayilar3);
+  print(sayilar3.length);
+
+  List<int> sayilar4 = List.empty(growable: true);
+  List<int> sayilar5 = [];
+  // bu üstteki ikisi aynı aslında.
+  sayilar4.add(1);
+  sayilar5.add(1);
+  print(sayilar4);
+}
---
+++ bolum4/sabit_boyutlu_listeler.dart
@@ -0,0 +1,16 @@
+void main(List<String> args) {
+  int sayi = 4;
+  List<int> sayilar = List.filled(5, 2, growable: false);
+  sayilar[0] = 4;
+  sayilar[1] = 1;
+  sayilar[2] = 9;
+  print(sayilar);
+  print(sayilar.length);
+
+  List<dynamic> karisik = List<dynamic>.filled(5, 0);
+  karisik[0] = 'fer';
+  karisik[1] = 5;
+  karisik[3] = true;
+
+  print(karisik);
+}
