3a8d496fedc1441a546691271cbfebfe756a9d4a
fkond <fkond@yahoo.com>
2023-05-28 08:40:10 +0300

    çok açıklayıcı yorum satırları eklendi.

---
+++ odev/cember_daire.dart
@@ -0,0 +1,25 @@
+// nesnesel_odev.dart dosyasından CemberDaire() veri girildiğinde aşağısı çalışır.
+class CemberDaire {
+  int _yariCap = 1; // null gelmesin diye.
+  double _pi = 3.14; // sabit zaten.
+
+  CemberDaire(int gelenCap) { // 1. Adım program burdan başlar
+    _yariCapKontrol = gelenCap; // 2. Adım gelen değer _yariCapKontrol değişkenine atandı
+  }
+
+  void set _yariCapKontrol(int capKontrol) { // 3. Adım gelen değer capKontrol değerine atandı
+    if (capKontrol > 0) {
+      _yariCap = capKontrol; // 4. Adım eğer 0'dan büyükse _yariCap a atanmaya hak kazandı.
+    } else {
+      _yariCap = 1; // 4. Adım eğer 0'dan küçükse ihmal edildi ve 1 değeri ile yola devam.
+    }
+  }
+
+  double cevreHesapla() {
+    return 2 * _pi * _yariCap; // 5. Adım cevreHesapla fonksiyonu çalıştı ve sonuç dönüldü.
+  }
+
+  double alanHesapla() {
+    return _pi * _yariCap * _yariCap; // 5. Adım alanHesapla fonksiyonu çalıştı ve sonuç dönüldü.
+  }
+}
--- odev/nesnesel_odev.dart
+++ odev/nesnesel_odev.dart
@@ -1,9 +1,13 @@
 // 1- CemberDaire sınıfı, kurucusu yarıçap ve alan. Ayrıca çevre ve alan hesaplayan metotlar olacak.
 
-// 2-  Ogrenci sınıfı, id ve not tutulacak, 
+// 2-  Ogrenci sınıfı, id ve not tutulacak,
 // 100 elemanlı bir listede id ve not değerleri rasgele üretilecek
 // ve bu öğrenciler saklanacak ve ekrana yazdırılacak.
 
+import 'cember_daire.dart';
+
 void main(List<String> args) {
-  
+  CemberDaire c1 = CemberDaire(4);
+  print("Alan: ${c1.alanHesapla()}");
+  print("Çevre: ${c1.cevreHesapla()}");
 }
