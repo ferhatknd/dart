a9d35bec65365c0ab7b0b7dce00a67c384a53620
fkond <fkond@yahoo.com>
2023-06-01 23:51:37 +0300

    asenkron işlemler ornek

--- bolum9/exception_kavrami.dart
+++ bolum9/exception_kavrami.dart
@@ -1,6 +1,14 @@
 void main(List<String> args) {
   print("Program başladı");
-  double sayi = 100 / 6;
-  print(sayi);
+  try {
+    int sayi = 100 ~/ int.parse("fer"); // bu bölen 0 ise başka hata olur.
+    print(sayi);
+  } on IntegerDivisionByZeroException {
+    print("Bölen sıfır olamaz");
+  } on FormatException catch (e) {
+    print("Hata: ${e.message} \ ${e.source}");
+  } finally {
+    print("işlem bitti");
+  }
   print("Program bitti");
 }
